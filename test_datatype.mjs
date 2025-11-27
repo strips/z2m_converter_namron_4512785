87f8e15aafad92da0af1512a674389a7a3c88618
strips <reg.gitpub@stians.com>
2025-11-27 19:54:59 +0100

    Remove test_datatype.mjs file as it is no longer needed

--- test_datatype.mjs
+++
@@ -1,6 +0,0 @@
-import zcl from 'zigbee-herdsman/dist/zspec/zcl/index.js';
-const {DataType} = zcl;
-console.log('DataType:', DataType);
-console.log('DataType.enum8:', DataType?.enum8);
-console.log('DataType keys:', Object.keys(DataType || {}).slice(0, 10));
- 
